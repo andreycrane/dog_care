e47bc81f2c05d52195a74d6fef466ded99d782f5
Andrey Zhuravlev <crane.andrey@gmail.com>
2020-06-08 21:35:48 +0300

    a bit of markup

--- posthtml.config.js
+++ posthtml.config.js
@@ -2,7 +2,37 @@ module.exports = {
   plugins: {
     'posthtml-include': {},
     'posthtml-expressions': {
-      locals: {},
+      locals: {
+        promises: [
+          {
+            img: '../img/dog_love_24x.svg',
+            title: '24/7 Love & Care',
+            text: `
+              At vero eos et accusamus et iusto odio
+              dignissimos ducimus qui blanditiis
+              praesentium corrupti quos dolores et quas.
+            `,
+          },
+          {
+            img: '../img/dog_vet_24x.svg',
+            title: 'On-site Veterinarian',
+            text: `
+              Dolorem ipsum quia dolor sit amet, consectetur,
+              adipisci velit, sed quia non numquam eius modi
+              tempora incidunt.
+            `,
+          },
+          {
+            img: '../img/dog_stream_24x.svg',
+            title: 'Live-stream Cameras',
+            text: `
+              Quis autem vel eum iure reprehenderit qui
+              in ea voluptate velit esse quam nihil molestiae
+              consequatur illum.
+            `,
+          },
+        ],
+      },
     },
   },
 };
